ed49cc1ee63ecd7e9e89f378858606b32fcb65d3
anjanragh <anjanragh@gmail.com>
2020-07-31 16:45:59 -0400

    Added montage creation

--- detect_parkinsons.py
+++ detect_parkinsons.py
@@ -13,6 +13,7 @@ import cv2
 
 dataset_path_spiral = "./dataset/spiral"
 dataset_path_wave = "./dataset/wave"
+dataset_testing_spiral = "./dataset/spiral/testing"
 
 
 def process_image(image):
@@ -66,9 +67,38 @@ def test_metrics(model, testX, testy):
     return metrics
 
 
+def create_montage(model, testingPath):
+    testingPaths = list(paths.list_images(testingPath))
+    idxs = np.arange(0, len(testingPaths))
+    idxs = np.random.choice(idxs, size=(25,), replace=False)
+    images = []
+    for i in idxs:
+        image = cv2.imread(testingPaths[i])
+        output = image.copy()
+        output = cv2.resize(output, (128, 128))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (200, 200))
+        image = cv2.threshold(image, 0, 255,
+                              cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        features = process_image(image)
+        preds = model.predict([features])
+        label = le.inverse_transform(preds)[0]
+        color = (0, 255, 0) if label == "healthy" else (0, 0, 255)
+        cv2.putText(output, label, (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    color, 2)
+        images.append(output)
+    montage = build_montages(images, (128, 128), (5, 5))[0]
+    cv2.imshow("Output", montage)
+    cv2.waitKey(0)
+
+
 if __name__ == "__main__":
     trainX, trainy, testX, testy = create_dataset(dataset_path_wave)
     print(len(trainX), len(trainy), len(testX), len(testy))
+    le = LabelEncoder()
+    trainy = le.fit_transform(trainy)
+    testy = le.transform(testy)
     model = train(trainX, trainy)
     metrics = test_metrics(model, testX, testy)
     print(metrics)
+    create_montage(model, dataset_testing_spiral)
